c50c26035ff97fac9b8b99b09890d89157edea1a
daniel-jin <daniel.h.jin@gmail.com>
2018-01-10 02:57:05 -0600

    Static table view

--- Alarm/Alarm/AlarmDetailTableViewController.swift
+++ Alarm/Alarm/AlarmDetailTableViewController.swift
@@ -9,21 +9,25 @@
 import UIKit
 
 class AlarmDetailTableViewController: UITableViewController {
+    
+    // MARK: - IBOutlets & IBActions
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var alarmNameTextField: UITextField!
+    @IBOutlet weak var enableButton: UIButton!
+    
+    @IBAction func enableButtonTapped(_ sender: Any) {
+        
+        
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     // MARK: - Table view data source
 
